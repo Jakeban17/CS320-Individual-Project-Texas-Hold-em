c85b9b3150f8158c3dd6b57f9903aa405c524088
Jakeban17 <jnielsen1@ycp.edu>
2024-03-16 11:41:39 -0400

    Added card pngs, created displayCard class, merged displayCard to main

--- main.js
+++ main.js
@@ -69,7 +69,42 @@ class Player{
     }
 }
 
+class DisplayCard {
+    constructor(){
+        this.cardValues = {
+            '2': "2", 
+            '3': "3", 
+            '4': "4", 
+            '5': "5", 
+            '6': "6", 
+            '7': "7", 
+            '8': "8", 
+            '9': "9", 
+            '10': "10", 
+            'Jack': "J", 
+            'Queen': "Q", 
+            'King': "K", 
+            'Ace': "A"
+        };
+        this.cardSuits = {
+            'Clubs': "C", 
+            'Spades': "S", 
+            'Hearts': "H", 
+            'Diamonds': "D", 
+        };
+    }
 
+    findFileName(card){
+        var cardValue = card.value;
+        var cardSuit = card.suit;
+
+        if (cardValue in this.cardValues && cardSuit in this.cardSuits) {
+            return "PlayingCards/" + this.cardValues[cardValue] + this.cardSuits[cardSuit] + ".png";
+        } else {
+            return "PlayingCards/Card.png";
+        }
+    }
+}
 
 function main() {
     Deck.createDeck();
